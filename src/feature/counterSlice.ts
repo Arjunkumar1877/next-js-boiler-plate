4a81e088539424c46f7fbcc6684d86c7b2f15809
Arjunkumar1877 <arjun.tech177@gmail.com>
2024-12-22 01:28:05 +0530

    chore: setup redux with persistor

---
+++ src/feature/counterSlice.ts
@@ -0,0 +1,29 @@
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+
+interface CounterState {
+  value: number;
+}
+
+const initialState: CounterState = {
+  value: 0,
+};
+
+const counterSlice = createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment: (state: { value: number }) => {
+      state.value += 1;
+    },
+    decrement: (state: { value: number }) => {
+      state.value -= 1;
+    },
+    incrementByAmount: (state: { value: any }, action: PayloadAction<number>) => {
+      state.value += action.payload;
+    },
+  },
+});
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+
+export default counterSlice.reducer;
